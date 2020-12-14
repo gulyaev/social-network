1089e85dedc68800558efdd81136f4b1a84c7515
gulyaev <gulyaev.gvs@gmail.com>
2020-12-14 14:16:06 +0200

    45 version of social-network done. Connect / MapStateToProps /  MapDispatchToProps

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
-import StoreContext from "../../../StoreContext";
+import {connect} from "react-redux";
 
 
-const MyPostsContainer = () => {
-    return (
-        <StoreContext.Consumer>
-            { (store)=>{
-                let state = store.getState();
-
-                let addPost = () => {
-                    //props.addPost();
-                    //let text = newPostElement.current.value;
-                    //alert('samuraijs.com');
-                    store.dispatch(addPostActionCreator());
-                    //newPostElement.current.value='';//зануляем - очищаем поле
-                    //props.updateNewPostText(' ');
-                }
-
-                let onPostChange = (text) => {
-                    let action = updateNewPostTextActionCreator(text);
-                    store.dispatch(action);
-                }
-
-                return <MyPosts updateNewPostText={onPostChange}
-                     addPost={addPost}
-                     posts={state.profilePage.posts}
-                     newPostText={state.profilePage.newPostText}
-            />
-            }
-        }
-        </StoreContext.Consumer>
-    )
+const mapStateToProps = (state) => {
+    return {
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+    }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (text)=>{
+            let action = updateNewPostTextActionCreator(text);
+            dispatch(action);
+        },
+
+        addPost: ()=>{
+            dispatch(addPostActionCreator());
+        }
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
